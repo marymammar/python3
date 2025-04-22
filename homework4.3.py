a2fe2a0011335abba96f827f3760af5e1dc3cd44
marymammar <138240838+marymammar@users.noreply.github.com>
2025-04-22 13:08:04 -0400

    Update homework4.3.py

--- homework4.3.py
+++ homework4.3.py
@@ -1,1 +1,12 @@
 
+import re
+
+filename = open('../market_discussion.txt')
+file= filename.read()
+
+pattern = r'[A-Z]+, [+-]\d+\.\d{2}\%'
+matchboth= re.findall(pattern,file)
+matchboth.sort()
+
+for match in matchboth:
+    print(match)
