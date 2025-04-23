2f69a208d637693ba45de0367be8ae32626c0586
marymammar <138240838+marymammar@users.noreply.github.com>
2025-04-23 08:50:36 -0400

    Update homework4.2.py

--- homework4.2.py
+++ homework4.2.py
@@ -1,1 +1,25 @@
-homework 3
+
+import re
+
+file= '../access_log.txt'
+filename = open(file)
+bytes_dict = {}
+for line in filename:
+    matchid = re.search(r'~([a-zA-z][a-zA-z][a-zA-Z]?\d+)',line)
+    matchbytes = re.search(r'(\d+)',line)
+
+    if not matchid or not matchbytes:
+        continue
+    stuid = matchid.group(1)
+    bytes = int(matchbytes.group(1))
+
+    if stuid not in bytes_dict:
+        bytes_dict[stuid]= 0
+    bytes_dict[stuid] = bytes_dict[stuid] + bytes
+
+filename.close()
+
+for key in sorted(bytes_dict, key=bytes_dict.get, reverse= True):
+    if bytes_dict[key] > 7000:
+        print(f'{key}:  {bytes_dict[key]}')
+
