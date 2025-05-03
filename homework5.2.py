89924d7f35ed8cdf5ced47ae46f64822dd73cd9c
marymammar <138240838+marymammar@users.noreply.github.com>
2025-05-03 14:42:03 -0400

    Update homework5.2.py

--- homework5.2.py
+++ homework5.2.py
@@ -1,23 +1,27 @@
-filename = '../dated_file.csv'
-fname = '../sorted_file.csv'
 
-def line_by_date(thisline):
-    lines = thisline.split()
-    date = lines[0]
-    return date
-
-fh = open(filename)
-readlines = fh.readlines()
-
-# before converting to lambda
-#slines = sorted(readlines, key = line_by_date, reverse = True)
-
-# convert to lambda
-slines = sorted(readlines, key = lambda thisline: thisline.split()[0],  reverse = True)
-wfh = open(fname, 'w', newline='')
-
-for line in slines:
-    wfh.write(line)
-wfh.close()
-
-print(f'wrote to {fname}')
+  1    from datetime import datetime
+  2    
+  3    filename = '../dated_file.csv'
+  4    fname = '../sorted_file.csv'
+  5    
+  6    
+  7    def line_by_date(thisline):
+  8        lines = thisline.split()
+  9        date = lines[0]
+ 10        return date
+ 11    
+ 12    fh = open(filename)
+ 13    readlines = fh.readlines()
+ 14    fh.close()
+ 15    # before converting to lambda
+ 16    #slines = sorted(rreadlineseadlines, key = line_by_date, reverse = True)
+ 17    
+ 18    # convert to lambda
+ 19    slines = sorted(readlines, key = lambda thisline: datetime.strptime(thisline.split(',')[0], '%m/%d/%Y'))
+ 20    wfh = open(fname, 'w', newline='')
+ 21    
+ 22    for line in slines:
+ 23        wfh.write(line)
+ 24    wfh.close()
+ 25    
+ 26    print(f'wrote to {fname}')
